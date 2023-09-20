58b0bc43576425a27f3e562c76e0687236842a77
Yaqub Mahmoud <yaqub320@gmail.com>
2023-09-20 14:39:33 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<script>
+<script lang='ts'>
+	import type { PageData } from "./$types";
+	export let data: PageData;
+	const clientIP: string = data.body?.clientIP!;
+	const clientCountry: string = data.body?.country;
 </script>
 
 <svelte:head>
@@ -7,6 +11,10 @@
 </svelte:head>
 
 <div>
-	<p>Hello!</p>
-	<p>Note: IP of "::1" means you are running on localhost. Deploy to server to see true IP.</p>
+	<p>Client IP Address: {clientIP}</p>
+	<p>IP Location: {clientCountry}</p>
+	{#if clientCountry === undefined}
+		<p>Note: IP of "::1" and Location of "undefined" means you are running on localhost.</p>
+		<p>Deploy to server to see true IP.</p>
+	{/if}
 </div>
